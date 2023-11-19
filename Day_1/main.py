2b70a5918511e79e0d0d0376aef09a77ef626aeb
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-11-19 17:29:36 +0000

    complete day 1 challenge

---
+++ Day_1/main.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator')
+city = input("What's name of the city your grew up in?\n")
+pet_name = input('What is your pet name?\n')
+print(f'Your band name could be {city} {pet_name}')
