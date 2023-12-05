b708d9beed919beec36712f21f0e9e2000177b73
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-12-05 20:48:42 +0000

    complete day 17 challenge

---
+++ day_17/data.py
@@ -0,0 +1,43 @@
+question_data = [
+    {
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Android versions are named in alphabetical order.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "All program codes have to be compiled into an executable file in order to be run. This file can "
+        "then be executed on any machine.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+    },
+]
---
+++ day_17/main.py
@@ -0,0 +1,18 @@
+from data import question_data
+from random import choice
+
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    new_question = Question(question["question"], question["correct_answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+quiz.print_final_score()
