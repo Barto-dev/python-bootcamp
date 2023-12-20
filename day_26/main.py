cb1d2fb3a659800363750101d6b5b864a9154892
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-12-20 12:42:43 +0000

    day 30: add error handling in day_26 project

--- day_26/main.py
+++ day_26/main.py
@@ -4,6 +4,16 @@ alphabet_data_raw = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dataframe = pandas.DataFrame(alphabet_data_raw)
 phonetic_dict = {row.letter: row.code for (index, row) in alphabet_dataframe.iterrows()}
 
-user_input = input("Enter a word: ").upper()
-phonetic_translation = [phonetic_dict[letter] for letter in user_input]
-print(phonetic_translation)
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        phonetic_translation = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_translation)
+
+
+generate_phonetic()
