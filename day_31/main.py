b9f5c004c74bed800d597ac74729d189ccf9b19a
Illia <60040628+Barto-dev@users.noreply.github.com>
2023-12-22 16:39:51 +0000

    day 31: refactor FlashCardApp

--- day_31/main.py
+++ day_31/main.py
@@ -1,3 +1,4 @@
+import logging
 from tkinter import *
 import pandas
 from random import choice
@@ -6,68 +7,94 @@ BACKGROUND_COLOR = "#B1DDC6"
 FONT = "Arial"
 
 
-def load_data():
-    try:
-        return pandas.read_csv("data/words_to_learn.csv")
-    except FileNotFoundError:
-        return pandas.read_csv("data/french_words.csv")
+class FlashCardApp:
+    def __init__(self):
+        self.known_button = None
+        self.check_img = None
+        self.unknown_button = None
+        self.cross_img = None
+        self.card_word = None
+        self.card_title = None
+        self.card_background = None
+        self.card_back_img = None
+        self.card_front_img = None
+        self.canvas = None
+        self.window = None
+        self.current_card = {}
+        self.to_learn = self.load_data()
+        self.flip_timer = None
+
+    def load_data(self, file_name="data/words_to_learn.csv"):
+        try:
+            return pandas.read_csv(file_name).to_dict(orient="records")
+        except FileNotFoundError:
+            logging.exception("File not found, loading default data")
+            return pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+
+    def save_data(self, file_name="data/words_to_learn.csv"):
+        pandas.DataFrame(self.to_learn).to_csv(file_name, index=False)
+
+    def next_card(self):
+        self.window.after_cancel(self.flip_timer)
+        self.current_card = choice(self.to_learn)
+        self.canvas.itemconfig(self.card_title, text="French", fill="black")
+        self.canvas.itemconfig(
+            self.card_word, text=self.current_card["French"], fill="black"
+        )
+        self.canvas.itemconfig(self.card_background, image=self.card_front_img)
+        self.flip_timer = self.window.after(3000, func=self.flip_card)
+
+    def is_known(self):
+        self.to_learn.remove(self.current_card)
+        self.save_data()
+        self.next_card()
+
+    def flip_card(self):
+        self.canvas.itemconfig(self.card_title, text="English", fill="white")
+        self.canvas.itemconfig(
+            self.card_word, text=self.current_card["English"], fill="white"
+        )
+        self.canvas.itemconfig(self.card_background, image=self.card_back_img)
+
+    def run(self):
+        self.window = Tk()
+        self.window.title("Flashy")
+        self.window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+        self.flip_timer = self.window.after(3000, func=self.flip_card)
+        self.canvas = Canvas(width=800, height=526)
+        self.card_front_img = PhotoImage(file="images/card_front.png")
+        self.card_back_img = PhotoImage(file="images/card_back.png")
+        self.card_background = self.canvas.create_image(
+            400, 263, image=self.card_front_img
+        )
+        self.card_title = self.canvas.create_text(
+            400, 150, text="Title", font=(FONT, 40, "italic")
+        )
+        self.card_word = self.canvas.create_text(
+            400, 263, text="word", font=(FONT, 60, "bold")
+        )
+        self.canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+        self.cross_img = PhotoImage(file="images/wrong.png")
+        self.unknown_button = Button(
+            image=self.cross_img,
+            highlightthickness=0,
+            borderwidth=0,
+            command=self.next_card,
+        )
+        self.unknown_button.grid(row=1, column=0)
+        self.check_img = PhotoImage(file="images/right.png")
+        self.known_button = Button(
+            image=self.check_img,
+            highlightthickness=0,
+            borderwidth=0,
+            command=self.is_known,
+        )
+        self.known_button.grid(row=1, column=1)
+        self.next_card()
+        self.window.mainloop()
 
 
-data = load_data()
-to_learn = data.to_dict(orient="records")
-current_card = {}
-
-
-def next_card():
-    global current_card
-    global flip_timer
-    window.after_cancel(flip_timer)
-    current_card = choice(to_learn)
-    canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-    canvas.itemconfig(card_background, image=card_front_img)
-    flip_timer = window.after(3000, func=flip_card)
-
-
-def is_known():
-    to_learn.remove(current_card)
-    data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
-    next_card()
-
-
-def flip_card():
-    canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
-    canvas.itemconfig(card_background, image=card_back_img)
-
-
-window = Tk()
-window.title("Flashy")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-flip_timer = window.after(3000, func=flip_card)
-
-canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
-card_background = canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="Title", font=(FONT, 40, "italic"))
-card_word = canvas.create_text(400, 263, text="word", font=(FONT, 60, "bold"))
-canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0, columnspan=2)
-
-cross_img = PhotoImage(file="images/wrong.png")
-unknown_button = Button(
-    image=cross_img, highlightthickness=0, borderwidth=0, command=next_card
-)
-unknown_button.grid(row=1, column=0)
-
-check_img = PhotoImage(file="images/right.png")
-known_button = Button(
-    image=check_img, highlightthickness=0, borderwidth=0, command=is_known
-)
-known_button.grid(row=1, column=1)
-
-next_card()
-window.mainloop()
+if __name__ == "__main__":
+    app = FlashCardApp()
+    app.run()
